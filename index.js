e518e3c634a62afff3fe65479af94ba73fb10b9f
Stefan Isak <s.isak@zimmer7.com>
2016-11-27 23:34:29 +0100

    Kickstart layout and user model including login/logout

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const dotenv = require('dotenv');
 const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 const passport = require('passport');
+const flash = require('express-flash');
 const expressStatusMonitor = require('express-status-monitor');
+const expressValidator = require('express-validator');
 const sass = require('node-sass-middleware');
 
 dotenv.load({ path: '.env.local' });
@@ -38,6 +40,8 @@ app.use(sass({
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(flash());
 app.use(session({
   resave: true,
   saveUninitialized: true,
@@ -68,13 +72,12 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 84600 }));
 
 app.get('/', homeController.index);
+app.get('/login', userController.getLogin);
+app.post('/login', userController.postLogin);
+app.get('/logout', userController.logout);
 
-/**
- * Start Express server.
- */
 app.listen(app.get('port'), () => {
   console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
   console.log('  Press CTRL-C to stop\n');
 });
-
 module.exports = app;
