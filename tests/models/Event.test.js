e94ca64a415e31c55a6b8c5c654582613c93e713
Stefan Isak <s.isak@zimmer7.com>
2017-01-31 19:11:34 +0100

    Add tests for url related virtual properties

---
+++ tests/models/Event.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const Event = require('../../lib/models/Event');
+const slug = require('../../lib/utilities/slug');
+
+describe('Event', () => {
+  beforeEach(() => {
+  });
+  afterEach(() => {
+  });
+  it('should have a valid virtual property editUrl', (done) => {
+    const event = new Event({
+      title: 'Foo Bar',
+      slug: slug.parse('Foo Bar')
+    });
+    assert.equal(
+      process.env.BASE + 'account/events/foo-bar',
+      event.editUrl
+    );
+    done();
+  });
+});
