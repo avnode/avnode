a6e255d8851c703f139d87630da4ffe450599cf1
Stefan Isak <s.isak@zimmer7.com>
2017-02-01 10:51:28 +0100

    Add confirm dialog

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,9 +1,4 @@
-$(document).on('click', '.ajax', function(ev) {
-  ev.preventDefault();
-  const $el = $(this);
-  const url = $el.attr('data-endpoint');
-  const method = $el.attr('data-method');
-  const payload = JSON.parse($el.attr('data-payload'));
+const request = (url, method, payload) => {
   $.ajax({
     url: url,
     method: method,
@@ -11,4 +6,27 @@ $(document).on('click', '.ajax', function(ev) {
   }).done(function() {
     window.location.reload();
   });
+}
+
+$(document).on('click', '.ajax', function(ev) {
+  ev.preventDefault();
+  const $el = $(this);
+  const url = $el.attr('data-endpoint');
+  const method = $el.attr('data-method');
+  const payload = JSON.parse($el.attr('data-payload'));
+  const confirm = $el.attr('data-confirm');
+  if (confirm) {
+    bootbox.confirm({
+      size: 'small',
+      message: 'Are you sure?',
+      closeButton: false,
+      callback: function(result) {
+        if (result) {
+          request(url, method, payload);
+        }
+      }
+    });
+  } else {
+    request(url, method, payload);
+  }
 });
